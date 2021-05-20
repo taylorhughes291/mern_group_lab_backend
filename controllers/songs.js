25dd48385f9d6d59d9af0dc95d442cef6a6ab9a2
Taylor Hughes <taylorhughes@Taylors-Air.lan>
2021-05-20 09:27:13 -0700

    completed routes

--- controllers/songs.js
+++ controllers/songs.js
@@ -3,14 +3,34 @@ const {Router} = require('express')
 const { findByIdAndUpdate } = require("../models/song")
 const router = Router()
 
-//test route
+//get/index route
 router.get('/', async (req, res) => {
+    const songs = await Song.find({})
     res.json({
         status: 200,
-        message: "You've hit the router"
+        data: songs
     })
 })
 
+//Post route
+router.post('/', async (req, res) => {
+    const body = req.body
+    const newSong = await Song.create(body)
+    res.json({
+        status: 200,
+        data: newSong
+    })
+})
+
+// Delete songs route
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    await Song.findByIdAndDelete(id)
+    res.json({
+        status: 200,
+        message: "Successfully deleted"
+    })
+})
 
 
 
