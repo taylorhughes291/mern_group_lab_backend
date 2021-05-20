3ea20582a92d490ad16f26cf864e209c0c3503df
Dong Geon Brian Park <brianpark@Dongui-MacBook-Pro.local>
2021-05-19 21:03:37 -0400

    first commit

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const {MONGODB_URL} = process.env;
+
+// connect
+mongoose.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+// message
+mongoose.connection
+.on("open", () => console.log("connected to mongo"))
+.on("close", () => console.log("disconnected to mongo"))
+.on("error", (error) => console.log(error))
+
+
+module.exports = mongoose
---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("./db/connection");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const { PORT=7777 } = process.env;
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(PORT, () => console.log(`port running on ${PORT}`));
